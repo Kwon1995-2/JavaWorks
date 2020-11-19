e4657264ba796947908886e95a085497d33acf33
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2020-11-19 21:17:45 +0900

    Add files via upload

--- gradle2/cpp_linkedlist.java
+++ gradle2/cpp_linkedlist.java
@@ -5,7 +5,8 @@ import java.util.*;
 class Node { //constructor
     private int num;
     public Node next;
-    public Node() {this.num = (int)Math.random()*10+1; next=null;} // --> same value assign
+    //default constructor
+    public Node() {this.num = (int)(Math.random()*10)+1; next=null;} // not right uses : (int)Math.random()*10+1
     public Node(int num) {this.num = num; next=null;}
     public int GetNum() {
         return num;
@@ -36,8 +37,11 @@ class List { //List a1 = new List();
         return this;
     };
     //addNode
-    public void addNode(int data) {
-        Node node = new Node(data); //plus behind
+    //public void addNode(int data)
+    //public void addNode()
+    public void addNode(int data) { //it's my style
+        //Node node = new Node(data); //plus front
+        Node node = new Node(data);
         node.next = first.next;
         first.next = node;
 //        Node temp = first;
@@ -52,9 +56,24 @@ class List { //List a1 = new List();
 //        }
 //        temp.next = node;
     };
+    public void addNode2(int data) { //plus node behind
+        Node node = new Node(data);
+        Node temp; Node follow;
+        if(first.next == null) {
+            first.next = node;
+        } else {
+            temp = first.next;
+            follow = temp;
+            while(temp != null) {
+                follow = temp;
+                temp = temp.next;
+            }
+            follow.next = node;
+        }
+    };
     //showList
     public void showList() {
-        int count = 0;
+        int count = 0; //node count check
         Node temp = first.next; //start : first -> 1 more output
         while(temp != null) {
             System.out.printf("node%d:%d  ", count, temp.GetNum());
@@ -64,13 +83,23 @@ class List { //List a1 = new List();
         }
         System.out.println();
     };
+// numbers of Node in List must be known
+//    public void sortList() {
+//      Node copy = this.first.next;
+//      Node follow = copy.next;
+//      while(follow != null) {
+//          if(copy.GetNum() > follow.GetNum()) {
+//
+//          }
+//      }
+//    };
 
-    public List mergeList(List l) {
+    public List mergeList(List l) { //just cross node merge
         Node p = this.first.next; //if Node p = this.first --> num of first is printed;
         Node f = p.next;
         Node q = l.first.next;
         Node ff = q.next;
-        while (p.next != null) { //p != null --> error
+        while (p.next != null) { //p != null --> error because p is null : p=null --> null.next : error
             p.next = q;
             q.next = f;
             p = f;
@@ -82,36 +111,106 @@ class List { //List a1 = new List();
     return this;
     };
 
+    //descending
+    public List mergesortList(List l) {
+        Node p; Node temp; //Node pf;
+        Node q; Node qf;
+        int decision = 0;
+        //bigger number setting
+        if(this.first.next.GetNum() >= l.first.next.GetNum()) {
+            p = this.first.next;
+            q = l.first.next;
+        } else {
+            p = l.first.next;
+            q = this.first.next;
+            decision = 1;
+        }
+        temp = p; //pf = p.next;
+        qf = q.next;
+/////////////////////////////////////////////////////////////////////////////////////
+//        Node start; //start point setting
+//        if(p.GetNum() >= q.GetNum()) {start = p; p = p.next;}
+//        else {start = q; q = q.next;}
+
+//        while(p != null) {
+//            if(p.GetNum() >= q.GetNum()) {
+//                //temp = p;
+//                p = p.next; pf = pf.next;
+//            } else {
+//                p.next = q;
+//                q.next = pf;
+//                p = pf;
+//                q = qf;
+//                pf = pf.next; qf = qf.next;
+//                if(pf == null) {p.next = q; break;}
+//                //if(qf == null) {q.next = p; break;}
+//            }
+/////////////////////////////////////////////////////////////////////////////////////
+        while(p.next != null) { // --> while(true) --> don't care
+            if(p.GetNum() >= q.GetNum()) { temp = p; p = p.next; //pf = pf.next;
+            } else {
+                temp.next = q; q.next = p; //linking
+                temp = p; q = qf; //reassign
+                p = p.next; qf = qf.next; //reassign
+                if(p.next == null) {
+                    temp.next = q; q.next = p;
+                    if(qf != null) p.next = qf;
+                    break;
+                    }
+                }
+            }
+        if(decision == 1) return l;
+        else return this;
+    };
+
 }
 
 public class cpp_linkedlist {
     public static Random rand = new Random();
     public static void MakeList(List l, int num) {
         int rnum;
+        if(num%2 == 0) rnum = 2;
+        else           rnum = 1;
         for(int i=0;i<num;i++) {
-            rnum = rand.nextInt(10) + 1;
+            //rnum = rand.nextInt(10) + 1;
             //l.addNode();
             l.addNode(rnum);
+            //l.addNode2(rnum);
+            rnum += 2;
         }
     }
 
     public static void main(String[] args) {
-        List a1 = new List();
+        List a1 = new List(); //reference variable : a1 <- address
         List a2 = new List();
         MakeList(a1, 10); //10: number of Node
         MakeList(a2,5);
         a1.showList();
         System.out.println();
+
         a2.showList();
         System.out.println();
-        //a1.addList(a2).showList();
+
+//        addList test
+//        a1.addList(a2).showList();
 //        List a3 = a1.addList(a2);
 //        a3.showList();
 //        System.out.println();
-//        List a4 = a1.mergeList(a2);
+
+        //mergeList test;
+//        List a4 = a1.mergeList(a2); //a1 is changed
 //        a4.showList();
-        List a5 = a2.mergeList(a1);
-        a5.showList();
+//        System.out.println();
+//        List a5 = a2.mergeList(a1);
+//        a5.showList();
+
+
+        //mergesortList test
+        List a6 = a1.mergesortList(a2);
+        a6.showList();
+//        List a7 = a2.mergesortList(a1);
+//        a7.showList();
+
 //        List a5;
 //        a5 = a4;
 
