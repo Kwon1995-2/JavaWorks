ce8f8d83f1747ce6e7c7768e6c64f2f69afa07d8
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2020-11-24 21:36:35 +0900

    Add files via upload

--- gradle2/cpp_linkedlist.java
+++ gradle2/cpp_linkedlist.java
@@ -15,28 +15,38 @@ class Node { //constructor
 
 class List { //List a1 = new List();
     private Node first;
+
     //default constructor
-    public Node GetFirst() {return first;}
-    public void SetFirst(Node first) {this.first = first;}
+    public Node GetFirst() {
+        return first;
+    }
+
+    public void SetFirst(Node first) {
+        this.first = first;
+    }
+
     public List() {
-       this.first = new Node();
+        this.first = new Node();
     };
+
     //overloading constructor
     public List(int num) {
         this.first = new Node(num);
     };
+
     //copy constructor --> retry
     public List(List l) {
         this.first = new Node(l.first.GetNum());
         Node m = this.first;
         Node p = l.first.next;
-        while(p != null) {
+        while (p != null) {
             Node nd = new Node(p.GetNum());
             m.next = nd;
             m = nd;
             p = p.next;
         }
     }
+
     //addList
 //    public List addList(List l) {
 //        Node temp = this.first;
@@ -48,11 +58,11 @@ class List { //List a1 = new List();
 //    };
     public List addList(List l) {
         Node temp = this.first;
-        while(temp.next != null) {
+        while (temp.next != null) {
             temp = temp.next;
         }
         Node p = l.first.next; //only parameter copy constructor??
-        while(p !=null) {
+        while (p != null) {
             Node nd = new Node(p.GetNum());
             temp.next = nd;
             temp = nd;
@@ -60,6 +70,7 @@ class List { //List a1 = new List();
         }
         return this;
     };
+
     //addNode
     //public void addNode(int data)
     //public void addNode()
@@ -79,34 +90,44 @@ class List { //List a1 = new List();
 //            temp = temp.next;
 //        }
 //        temp.next = node;
-    };
+    }
+
+    ;
+
     public void addNode2(int data) { //plus node behind
         Node node = new Node(data);
-        Node temp; Node follow;
-        if(first.next == null) {
+        Node temp;
+        Node follow;
+        if (first.next == null) {
             first.next = node;
         } else {
             temp = first.next;
             follow = temp;
-            while(temp != null) {
+            while (temp != null) {
                 follow = temp;
                 temp = temp.next;
             }
             follow.next = node;
         }
-    };
+    }
+
+    ;
+
     //showList
     public void showList() {
         int count = 0; //node count check
         Node temp = first.next; //start : first -> 1 more output
-        while(temp != null) {
+        if (temp == null) return;
+        while (temp != null) {
             System.out.printf("node%d:%d  ", count, temp.GetNum());
             //System.out.println("node:"+temp.GetNum());
             temp = temp.next;
             count++;
         }
         System.out.println();
-    };
+    }
+
+    ;
 // numbers of Node in List must be known
 //    public void sortList() {
 //      Node copy = this.first.next;
@@ -128,20 +149,30 @@ class List { //List a1 = new List();
             q.next = f;
             p = f;
             q = ff;
-            f = f.next; ff = ff.next;
-            if(q.next == null) { p.next = q; q.next = f; break; }
-            if(p == null) { p.next = q; break; }
+            f = f.next;
+            ff = ff.next;
+            if (q.next == null) {
+                p.next = q;
+                q.next = f;
+                break;
+            }
+            if (p == null) {
+                p.next = q;
+                break;
+            }
         }
-    return this;
+        return this;
     };
 
     //descending
     public List mergesortList(List l) {
-        Node p; Node temp; //Node pf;
-        Node q; Node qf;
+        Node p;
+        Node temp; //Node pf;
+        Node q;
+        Node qf;
         int decision = 0;
         //bigger number setting
-        if(this.first.next.GetNum() >= l.first.next.GetNum()) {
+        if (this.first.next.GetNum() >= l.first.next.GetNum()) {
             p = this.first.next;
             q = l.first.next;
         } else {
@@ -170,25 +201,139 @@ class List { //List a1 = new List();
 //                //if(qf == null) {q.next = p; break;}
 //            }
 /////////////////////////////////////////////////////////////////////////////////////
-        while(p.next != null) { // --> while(true) --> don't care
-            if(p.GetNum() >= q.GetNum()) { temp = p; p = p.next; //pf = pf.next;
+        while (p.next != null) { // --> while(true) --> don't care  while (p.next != null)
+            if (p.GetNum() >= q.GetNum()) {
+                temp = p;
+                p = p.next; //pf = pf.next;
             } else {
-                temp.next = q; q.next = p; //linking
-                temp = p; q = qf; //reassign
-                p = p.next; qf = qf.next; //reassign
-                if(p.next == null) {
-                    temp.next = q; q.next = p;
-                    if(qf != null) p.next = qf;
+                temp.next = q;
+                q.next = p; //linking
+                temp = p;
+                q = qf; //reassign
+                p = p.next;
+                qf = qf.next; //reassign
+                if (p.next == null) {
+                    temp.next = q;
+                    q.next = p;
+                    if (qf != null) p.next = qf;
                     break;
-                    }
                 }
             }
-        if(decision == 1) return l;
+        }
+        if (decision == 1) return l;
+        else return this;
+    };
+
+    public int GetCount() {
+        Node p = this.first.next;
+        int count = 0;
+        while (p != null) {
+            p = p.next;
+            count++;
+        }
+        return count;
+    };
+
+    public List[] Split(List[] l) { //only 2 split -- only even
+        if (l[0] == null) {
+            l[0] = this;
+            return l;
+        }
+        List[] ll = new List[l.length]; //8
+        int size;
+        int count;
+        Node p = this.first.next;
+        for (int i = 0; i < l.length; i = i + 2) {
+            if (l[(int) i / 2] == null) break;
+            ll[i] = new List(this.GetFirst().GetNum());
+            ll[i + 1] = new List(this.GetFirst().GetNum());
+            //int idx = i / 2; --> half doesn't seen
+            size = l[i/2].GetCount() / 2; //4
+            count = 0;
+            while (count != size) {
+                ll[i].addNode2(p.GetNum());
+                p = p.next;
+                count++;
+            }
+            count = 0;
+            //if (size % 2.0 != 0) size--; // --> odd but doesn't work
+            while (count != size) {
+                ll[i + 1].addNode2(p.GetNum());
+                p = p.next;
+                count++;
+            }
+            //ll[i+1].first.next = p;
+        }
+        return ll;
+    };
+
+    public List[] mergesort(List[] l) {
+        List[] ll = new List[l.length]; //8
+        int size = l[0].GetCount();
+        int count;
+        for (int i = 0; i < l.length; i = i + 2) {
+            if(l[i] != null ) {
+                if(l[i+1] == null) ll[i/2] = l[i];
+                else ll[(int)i/2] = new List(l[i].Sort(l[i+1]));
+            }
+            //ll[i/2] = l[i].mergesortList(l[i+1]);
+            //ll[i/2] = new List(ll[i/2].Sort());
+        }
+        return ll;
+    };
+
+    public List Sort(List l) {
+        Node p; Node pf;
+        Node q; Node qf;
+        Node ret;
+        int decision = 0;
+        //bigger number setting
+        if (this.first.next.GetNum() <= l.first.next.GetNum()) {
+            p = this.first.next; ret = p;
+            q = l.first.next;
+        } else {
+            p = l.first.next; ret = p;
+            q = this.first.next;
+            decision = 1;
+        }
+        pf = p;
+        qf = q;
+        do {
+            if(ret == p) {
+                if (pf.GetNum() <= q.GetNum()) {
+                    pf = p;
+                    p = p.next;
+                    if (p == null) {
+                        p.next = q;
+                        break;
+                    }
+                } else {
+                    pf = p;
+                    p = p.next;
+                    ret.next = q;
+                    ret = q;
+                    }
+                } else if(ret == q) {
+                if (qf.GetNum() <= p.GetNum()) {
+                    qf = q;
+                    q = q.next;
+                    if (pf == null) {
+                        p.next = q;
+                        break;
+                    }
+                } else {
+                    q = qf; qf = qf.next;
+                    ret.next = p;
+                    ret = p;
+                    }
+                }
+            }while (qf != null);
+        if (decision == 1) return l;
         else return this;
     };
 
 }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class ListIterator {
     private List list;
     private Node current;
@@ -245,7 +390,7 @@ class ListIterator {
             temp = temp.next;
             //l.Next();
         }
-    };
+    }
 }
 
 public class cpp_linkedlist {
@@ -302,18 +447,6 @@ public class cpp_linkedlist {
         return sum/count;
     }
 
-    public static List[] split(List[] l, List[] r) {
-        List [] al = new List[size];
-        Node p = l.GetFirst().next;
-        for(int i=0;i<size;i++)
-        {
-            al[i].SetFirst(p);
-            System.out.println(p.GetNum());
-            p = p.next;
-        }
-        return al;
-    }
-
     public static void main(String[] args) {
         List a1 = new List(); //reference variable : a1 <- address
         //List a2 = new List();
@@ -321,8 +454,49 @@ public class cpp_linkedlist {
         //MakeList(a2,5);
         MakeList(a1, 8);
         a1.showList();
-        System.out.println();
-        split(a1,a3);
+        System.out.println("################# Split Begin #################");
+        int size = a1.GetCount();
+        List [] al1 = new List[size];
+        List [] al2 = a1.Split(al1);
+        for(int i=0;i<size;i++) {
+            if(al1[i] == null) continue;
+            al1[i].showList();
+        }
+        List [] al3 = a1.Split(al2);
+        for(int i=0;i<size;i++) {
+            if(al3[i] == null) continue;
+            al3[i].showList();
+        }
+        List [] al4 = a1.Split(al3);
+        for(int i=0;i<size;i++) {
+            if(al4[i] == null) continue;
+            al4[i].showList();
+        }
+        List [] al5 = a1.Split(al4);
+        for(int i=0;i<size;i++) {
+            if(al5[i] == null) continue;
+            al5[i].showList();
+        }
+        System.out.println("################# Mergesort Begin #################");
+        List[] al6 = a1.mergesort(al5);
+        for(int i=0;i<size;i++) {
+            if(al6[i] == null) continue;
+            al6[i].showList();
+        }
+
+        List[] al7 = a1.mergesort(al6);
+        for(int i=0;i<size;i++) {
+            if(al7[i] == null) continue;
+            al7[i].showList();
+        }
+
+        List[] al8 = a1.mergesort(al7);
+        for(int i=0;i<size;i++) {
+            if(al8[i] == null) continue;
+            al8[i].showList();
+        }
+        //System.out.println(num);
+
 //        a2.showList();
 //        System.out.println();
 
@@ -363,3 +537,4 @@ public class cpp_linkedlist {
 //        a5 = a4;
     }
 }
+
