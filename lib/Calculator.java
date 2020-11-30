4b87c534387d1cfc6dd9f84426db7aa9b7ee476d
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2020-11-30 21:29:30 +0900

    Add files via upload

---
+++ lib/Calculator.java
@@ -0,0 +1,7 @@
+package lib;
+
+public abstract class Calculator {
+    public abstract int add(int a, int b); // \B5\CE \C1\A4\BC\F6\C0\C7 \C7\D5\C0\BB \B1\B8\C7Ͽ\A9 \B8\AE\C5\CF
+    public abstract int subtract(int a, int b); // \B5\CE \C1\A4\BC\F6\C0\C7 \C2\F7\B8\A6 \B1\B8\C7Ͽ\A9 \B8\AE\C5\CF
+    public abstract double average(int[] a); // \B9迭\BF\A1 \C0\FA\C0\E5\B5\C8 \C1\A4\BC\F6\C0\C7 \C6\F2\B1\D5\C0\BB \B1\B8\C7\D8 \BDǼ\F6\B7\CE \B8\AE\C5\CF
+}
